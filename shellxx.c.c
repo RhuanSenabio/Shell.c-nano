8dfeb23376bd19b0cea99e7d4b5321b31901d7fe
RhuanSenabio <48081081+RhuanSenabio@users.noreply.github.com>
2019-07-05 12:58:57 -0300

    Update shellxx.c.c

--- shellxx.c.c
+++ shellxx.c.c
@@ -13,40 +13,33 @@ int main(){
    char usuario[30];
    char linhacomando[50];
    char nome_diretorio[100];
- 
-  
-	do{
+   
+    do{
 		             
-                getcwd(nome_diretorio, sizeof(nome_diretorio));
-		printf("%s~%s~>  ", getenv("USER"),nome_diretorio);
-                fgets(linhacomando, sizeof(linhacomando), stdin);
-                strtok(linhacomando,"\n");
-	//	linha = readline(">");
-		add_history(linhacomando);		
-		if( strcmp(linhacomando,"hello") == 0 ){
-		   system("clear");  
-		   printf("\nSeja bem vindo a minha SHELL!!!\n\n\n\n");
-		}else if( strcmp(linhacomando,"exit") == 0){
-    		   system("clear");  
-		   printf("\nENCERRANDO O PROGRAMA!!!\n\n\n");
-		}
-	       	else if( strstr(linhacomando,"cd") == linhacomando ){
-		   system("clear");
-		   chdir("..");
+           getcwd(nome_diretorio, sizeof(nome_diretorio));
+           printf("%s~%s~>  ", getenv("USER"),nome_diretorio);
 		
-	  	   printf("\nFaz alguma coisa!\n\n\n\n\n\n");
-		}else if( strcmp(linhacomando,"help") == 0){
-		    system("clear");  
-		    printf("\n\n -hello- Da saldacao ao usuario.\n -exit- Encerra o programa.\n -cd- Faz alguma coisa.\n -help- Mostra a tela de ajuda.\n\n\n\n");
-		} else{
-		   system("clear");
-		   printf("Comando invalido, tente outra vez!!!\n\n");
-		}
+           fgets(linhacomando, sizeof(linhacomando), stdin);
+           strtok(linhacomando,"\n");
+           add_history(linhacomando);
+		
+    	   if( strcmp(linhacomando,"hello") == 0 ){
+	   system("clear");  
+	   printf("\nSeja bem vindo a minha SHELL!!!\n\n\n\n");
+	   }else if( strcmp(linhacomando,"exit") == 0){
+    	        system("clear");  
+	        printf("\nENCERRANDO O PROGRAMA!!!\n\n\n");
+	   }else if( strstr(linhacomando,"cd") == linhacomando ){
+		system("clear");
+		chdir("..");
+	   }else if( strcmp(linhacomando,"help") == 0){
+	        system("clear");  
+    	        printf("\n\n -hello- Da saldacao ao usuario.\n -exit- Encerra o programa.\n -cd- Faz alguma coisa.\n -help- Mostra a tela de ajuda.\n\n\n\n");
+	   } else{
+	        system("clear");
+	        printf("Comando invalido, tente outra vez!!!\n\n");
+	   }
 
-	
-
-
-	}while( strcmp(linhacomando,"exit") != 0 );
-	
+     }while( strcmp(linhacomando,"exit") != 0 );
 
 }
